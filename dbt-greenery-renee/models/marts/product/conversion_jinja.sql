fe689b6d416ad43e339d505d9690a8622991ae92
Renee Liu <reneesyliu@gmail.com>
2021-11-25 04:25:36 +0000

    added one bespoke test

--- dbt-greenery-renee/models/marts/product/conversion_jinja.sql
+++ dbt-greenery-renee/models/marts/product/conversion_jinja.sql
@@ -3,21 +3,7 @@
         materialized = 'table'
     )
 }}
-
-/*
-with fact_conversion_agg as (
-select
-    sum(case when event_type = 'add_to_cart' then 1 end) as total_add_to_cart,
-    sum(case when event_type = 'checkout' then 1 end) as total_checkout,
-    sum(case when event_type = 'delete_from_cart' then 1 end) as total_delete_from_cart,
-    sum(case when event_type = 'package_shipped' then 1 end) as total_package_shipped,
-    sum(case when event_type = 'page_view' then 1 end) as total_page_view,
-    sum(case when event_type = 'account_created' then 1 end) as total_account_created
-from {{ ref('fact_events') }}
-)
-*/
-
-      
+    
 {% set event_types = ["page_view", "add_to_cart", "delete_from_cart", "checkout", "package_shippe", "account_created"] %}
 
 select
---
+++ dbt-greenery-renee/tests/assert_user_order_is_positive.sql
@@ -0,0 +1,5 @@
+select user_guid, 
+       sum(order_product_quantity) as total_order_quantity
+from {{ ref('fact_order') }}
+group by user_guid
+having not(sum(order_product_quantity) > 0)
