0c7a6ac374d5fa3c0bf7633d071254bdfcbac617
Renee Liu <reneesyliu@gmail.com>
2021-12-06 03:53:54 +0000

    week 3

--- dbt-greenery-renee/models/marts/product/prod_conversion_rate.sql
+++ dbt-greenery-renee/models/marts/product/prod_conversion_rate.sql
@@ -7,18 +7,17 @@
 
 with temp_prod as (
     select a.product_guid as product_guid, 
-           count_all as co_counts,
-           count_co as all_counts
+           count_all as all_counts,
+           count_co as co_counts
     from {{ ref('sessions_all') }} a
     join {{ ref('sessions_w_co')}} b
     on a.product_guid = b.product_guid
 )
 
-
 select * from temp_prod
 
 -- use this query to find out conversion rate by product
--- product_guid, co_counts/all_counts::decimal as conversion_rate from dbt_renee_liu.fin_conversion_by_prod;
+-- product_guid, co_counts/all_counts::decimal as conversion_rate from dbt_renee_liu.prod_conversion_by_prod;
 
 
 
